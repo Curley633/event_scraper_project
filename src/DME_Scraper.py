0f1411e657d735086c5d35b95db9edf7098b8a8b
James <curley633@hotmail.com>
2020-01-17 10:39:15 +0000

    Commit 9 - DME script complete and bash setup script updated

--- src/DME_Scraper.py
+++ src/DME_Scraper.py
@@ -23,56 +23,50 @@ while True:
         title = concert.find('a').text
         details = concert.find('p').text
         link = concert.find('a').get('href')
-        images = soup.find_all('div', {'class': 'featured-image'})
-        # for image in images:
-        # image = images.find('a').get('style')
-        # image = re.sub('background-image:url', '', image)
-        # image = re.sub('[(\';)]', '', image)
 
         event_no += 1
-        dme_events[event_no] = [title, details, link, images]
+        dme_events[event_no] = [title, details, link]
 
         # comment back in for testing
-        print('Event\n', title, '\nEvent Page:', link, '\nDetails:', details, '\nImage:', '\n---')
+        print('Event\n', title, '\nEvent Page:', link, '\nDetails:', details, '\n---')
 
     break
 
 print("Total new Events: ", event_no)
 
-# try:
-#     if os.path.exists(oldTablePath):
-#         os.remove(oldTablePath)
-#     dme_events_df = pd.DataFrame.from_dict(dme_events, orient='index', columns=['image', 'title', 'link'])
-#     dme_events_df.head()
-#     dme_events_df.to_csv('dme_events.csv')
-# except OSError:
-#     print("Can't delete file at this location: ..It may be open.", oldTablePath)
-#     print("EXITING")
-#     sys.exit(1)
-# conn = psycopg2.connect(dbname='EventScraper', user='postgres', password='curley', host='206.189.165.104', port='5432', sslmode='require')
-# cur = conn.cursor()
-#
-# # Comment back in for testing connection to postGreSQL
-# # print(conn.get_dsn_parameters(), "\n")
-# cur.execute("SELECT version();")
-# record = cur.fetchone()
-# print("You are connected to - ", record)
-#
-# cur.execute("""DROP TABLE IF EXISTS dme_events_table;
-# CREATE TABLE dme_events_table(
-# index int,
-# title varchar,
-# details varchar,
-# image varchar,
-# link varchar PRIMARY KEY)""")
-#
-# try:
-#     with open('dme_events.csv', encoding="utf8", mode='r') as f:
-#         next(f)
-#         cur.copy_from(f, 'dme_events_table', sep=',')
-#         conn.commit()
-#         print("Created new table in postgres, View Data using PGAdmin or HeidiSQL")
-# except FileNotFoundError:
-#     print("CSV File not found")
-#     print("EXITING")
-#     sys.exit(1)
+try:
+    if os.path.exists(oldTablePath):
+        os.remove(oldTablePath)
+    dme_events_df = pd.DataFrame.from_dict(dme_events, orient='index', columns=['title', 'details', 'link'])
+    dme_events_df.head()
+    dme_events_df.to_csv('dme_events.csv')
+except OSError:
+    print("Can't delete file at this location: ..It may be open.", oldTablePath)
+    print("EXITING")
+    sys.exit(1)
+conn = psycopg2.connect(dbname='EventScraper', user='postgres', password='curley', host='206.189.165.104', port='5432', sslmode='require')
+cur = conn.cursor()
+
+# Comment back in for testing connection to postGreSQL
+# print(conn.get_dsn_parameters(), "\n")
+cur.execute("SELECT version();")
+record = cur.fetchone()
+print("You are connected to - ", record)
+
+cur.execute("""DROP TABLE IF EXISTS dme_events_table;
+CREATE TABLE dme_events_table(
+index int,
+title varchar,
+details varchar,
+link varchar PRIMARY KEY)""")
+
+try:
+    with open('dme_events.csv', encoding="utf8", mode='r') as f:
+        next(f)
+        cur.copy_from(f, 'dme_events_table', sep=',')
+        conn.commit()
+        print("Created new DME table in postgres, View Data using PGAdmin or HeidiSQL")
+except FileNotFoundError:
+    print("CSV File not found")
+    print("EXITING")
+    sys.exit(1)
--- src/web_scraper_setup.sh
+++ src/web_scraper_setup.sh
@@ -5,3 +5,5 @@ python3 "src/Monroes_Scraper.py"
 python3 "src/Ticketmaster_Scraper.py"
 
 python3 "src/Blabbermouth_Scraper.py"
+
+python3 "src/DME_Scraper.py"
